a183649d5314ed2be8b3a19d1e902f9d4aec9dc9
Vikas Shah <vikas2003@gmail.com>
2014-11-20 19:36:37 +0000

    UserLookup

---
+++ app/domain/user/UserLookup.scala
@@ -0,0 +1,11 @@
+package domain.user
+
+object UserLookup {
+  val u = User(1, "abc@gmail.com", None)
+  
+  val listUsers = Map[Long, User]{u.id -> u,
+      2 -> u.copy(2, "2@gmail.com"),
+      3 -> u.copy(3, "3@gmail.com"),
+      4 -> u.copy(4)}
+  
+}
