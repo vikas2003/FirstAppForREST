277b8e8f5c7f2557b169267f5f097e19dbc12e03
Vikas Shah <vikas2003@gmail.com>
2014-11-20 19:41:46 +0000

    user

--- app/domain/user/User.scala
+++ app/domain/user/User.scala
@@ -1,4 +1,8 @@
 package domain.user
 
-case class User(val id: Option[Long],
-                val email: String)
+case class User(val id: Long,
+                val email: String,
+                val address: Option[Address])
+                
+case class Address(val firstLine: String, val secondLine: String,
+    val city: String, val zip: String)
--- app/domain/user/UserLookup.scala
+++ app/domain/user/UserLookup.scala
@@ -3,7 +3,7 @@ package domain.user
 object UserLookup {
   val u = User(1, "abc@gmail.com", None)
   
-  val listUsers = Map[Long, User]{u.id -> u,
+  val listUsers = Map{u.id -> u,
       2 -> u.copy(2, "2@gmail.com"),
       3 -> u.copy(3, "3@gmail.com"),
       4 -> u.copy(4)}
